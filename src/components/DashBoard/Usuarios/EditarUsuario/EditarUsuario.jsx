3d4d364e60cd41234e43fc9bd53fac5598d91026
cuervo357@gmail.com <cuervo357@gmail.com>
2021-09-15 08:30:41 -0500

    modulo editar usuario

---
+++ src/components/DashBoard/Usuarios/EditarUsuario/EditarUsuario.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from './EditarUsuario.module.css'
+
+function EditarUsuario() {
+  return (
+    <div>
+      Editar Usuario
+    </div>
+  )
+}
+
+export default EditarUsuario
