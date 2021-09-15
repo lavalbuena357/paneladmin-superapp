5580cecd020afae542fcbf16b56fd8b8057f6313
cuervo357@gmail.com <cuervo357@gmail.com>
2021-09-15 15:54:36 -0500

    ruta crear usuario

--- src/components/DashBoard/DashBoard.jsx
+++ src/components/DashBoard/DashBoard.jsx
@@ -1,16 +1,16 @@
 import React, { useState } from 'react'
 import style from './DashBoard.module.css'
-import { Route, useHistory } from "react-router-dom"
+import { Route } from "react-router-dom"
 import LeftPanel from './LeftPanel/LeftPanel'
 import Nav from './Nav/Nav'
 import Main from './Main/Main'
 import Usuarios from './Usuarios/Usuarios'
 import VerUsuario from './Usuarios/VerUsuario/VerUsuario'
+import EditarUsuario from './Usuarios/EditarUsuario/EditarUsuario'
+import CrearUsuario from './Usuarios/CrearUsuario/CrearUsuario'
 
 function DashBoard() {
   const [active, setActive] = useState('inicio')
-
-  const history = useHistory()
 
   return (
     <div className={`${style.dashboard} ${style.parent_div}`} style={{ backgroundColor: '#f7f7f7' }}>
@@ -27,8 +27,14 @@ function DashBoard() {
             <Route path='/inicio/usuarios'>
               <Usuarios />
             </Route>
+            <Route path='/inicio/crear-usuario'>
+              <CrearUsuario />
+            </Route>
             <Route path='/inicio/ver-usuario'>
               <VerUsuario />
+            </Route>
+            <Route path='/inicio/editar-usuario'>
+              <EditarUsuario />
             </Route>
           </div>
         </div>
